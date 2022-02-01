2d1bb17f34b72f3db3844927263fdff01d75d848
durruita <97204184+durruita@users.noreply.github.com>
2022-01-31 20:46:30 -0800

    Add files via upload

---
+++ shopping/script.js
@@ -0,0 +1,6 @@
+let item=prompt("What would you like to order?");
+let qty=prompt("How many Would you like?");
+let total= qty*2.00;
+let taxes= total*0.8;
+document.write(` Your total for ${qty} ${item} is ${total} thank you `);
+4
